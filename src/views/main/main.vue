1585e0d5bb5821905b6f92cb38b686c280e96572
qzfcoder <867303253@qq.com>
2023-02-18 15:53:08 +0800

    feat: router的集成

---
+++ src/views/main/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>main1111111111</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
