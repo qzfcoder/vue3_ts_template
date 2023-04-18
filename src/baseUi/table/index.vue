48c2309c66380d22102a5435b8e0b48e54e8b299
qzfcoder <867303253@qq.com>
2023-04-18 20:07:32 +0800

    feat: 封装table

--- src/baseUi/table/index.vue
+++ src/baseUi/table/index.vue
@@ -1,13 +1,117 @@
 <template>
-  <div></div>
+  <div>
+    <div class="header">
+      <slot name="header">
+        <div class="title">{{ title }}</div>
+      </slot>
+      <div class="handler">
+        <slot name="header-handler"></slot>
+      </div>
+    </div>
+    <el-table
+      :data="tableList"
+      :border="isBorder"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+      v-bind="childrenProps"
+    >
+      <el-table-column
+        v-if="showSelectCloum"
+        type="selection"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        v-if="showIndexCloum"
+        type="index"
+        label="序号"
+        width="80"
+        align="center"
+      ></el-table-column>
+      <template v-for="propItem in propList" :key="propItem.prop">
+        <el-table-column v-bind="propItem" align="center" show-overflow-tooltip>
+          <template #default="scope">
+            <slot :name="propItem.slotName" :row="scope.row">
+              {{ scope.row[propItem.prop] }}
+            </slot>
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
+    <div class="footer">
+      <slot name="footer">
+        <el-pagination
+          :currentPage="pageInfo.currentPage"
+          :page-sizes="[10, 20, 30, 40]"
+          :page-size="pageInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="listCount"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        >
+        </el-pagination>
+      </slot>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  setup() {
-    return {}
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    isBorder: {
+      type: Boolean,
+      default: false
+    },
+    tableList: {
+      type: Array,
+      required: true
+    },
+    propList: {
+      type: Array,
+      required: true
+    },
+    showIndexCloum: {
+      type: Boolean,
+      default: false
+    },
+    showSelectCloum: {
+      type: Boolean,
+      default: false
+    },
+    childrenProps: {
+      type: Object,
+      default: () => ({})
+    },
+    listCount: {
+      type: Number,
+      default: 0
+    },
+    pageInfo: {
+      type: Object,
+      default: () => ({
+        currentPage: 1,
+        pagesize: 10
+      })
+    }
+  },
+  emits: ['selectionChange', 'update:page'],
+  setup(props, { emit }) {
+    const handleSelectionChange = (value: any) => {
+      console.log(value)
+      emit('selectionChange', value)
+    }
+    const handleSizeChange = (val: any) => {
+      emit('update:page', { ...props.pageInfo, pagesize: val })
+    }
+    const handleCurrentChange = (val: any) => {
+      emit('update:page', { ...props.pageInfo, currentPage: val })
+    }
+    return { handleSelectionChange, handleSizeChange, handleCurrentChange }
   }
 })
 </script>
--- src/views/tableDemo/index.vue
+++ src/views/tableDemo/index.vue
@@ -1,15 +1,90 @@
 <template>
   <div>table</div>
+  <t-table
+    v-bind="contentTableConfig"
+    :tableList="tableList"
+    v-model:page="pageInfo"
+  >
+    <template #handle="scope">
+      <el-button @click="handleAction(scope)">处理</el-button>
+    </template>
+  </t-table>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import tTable from '@/baseUi/table/index.vue'
+const contentTableConfig = {
+  title: '用户列表',
+  propList: [
+    { prop: 'name', label: '商品名称', minWidth: '100' },
+    { prop: 'oldPrice', label: '原价', minWidth: '100', slotName: 'oldPrice' },
+    { prop: 'newPrice', label: '现价格', minWidth: '100' },
+    { prop: 'desc', label: '描述', minWidth: '100' },
+    { prop: 'status', label: '状态', minWidth: '100' },
+    { prop: 'imgUrl', label: '图片', minWidth: '100', slotName: 'imgUrl' },
+    { prop: 'inventoryCount', label: '发售', minWidth: '100' },
+    { prop: 'saleCount', label: '销售数量', minWidth: '100' },
+    {
+      prop: 'createAt',
+      label: '创建时间',
+      minWidth: '250',
+      slotName: 'createAt'
+    },
+    {
+      prop: 'updateAt',
+      label: '更新时间',
+      minWidth: '250',
+      slotName: 'updateAt'
+    },
+    {
+      label: '操作',
+      minWidth: '120',
+      slotName: 'handle'
+    }
+  ],
+  showIndexCloum: true,
+  showSelectCloum: true
+}
 
 export default defineComponent({
+  components: {
+    tTable
+  },
   setup() {
-    return {}
+    const pageInfo = ref({ currentPage: 1, pagesize: 10 })
+    const tableList = ref([{}])
+    const handleAction = () => {
+      console.log('处理')
+    }
+    watch(pageInfo, () => {
+      // getPageData()
+      console.log('重新获取数据信息，更新了页面', pageInfo.value)
+    })
+    return { tableList, contentTableConfig, pageInfo, handleAction }
   }
 })
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.header {
+  display: flex;
+  height: 45px;
+  padding: 0 5px;
+  justify-content: space-between;
+  align-items: center;
+  .title {
+    font-size: 24px;
+    font-weight: 700;
+  }
+  .handler {
+    align-items: center;
+  }
+}
+.footer {
+  margin-top: 15px;
+  .el-pagination {
+    text-align: right;
+  }
+}
+</style>
